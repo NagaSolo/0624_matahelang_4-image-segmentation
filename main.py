ccce16031a9df2a9ea7bc51809a8597e492a4fb6
Wan Ab Muadz Adzim <muadz.nagasolo@gmail.com>
2021-01-12 08:34:16 +0000

    added; streamlit module

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pixellib
 from pixellib.semantic import semantic_segmentation
 
 segment_image = semantic_segmentation()
-segment_image.load_pascalvoc_model("model/deeplabv3_xception_tf_dim_ordering_tf_kernels.h5") 
+segment_image.load_pascalvoc_model("model/deeplabv3_xception_tf_dim_ordering_tf_kernels.h5")
 segment_image.segmentAsPascalvoc(
     "tests/bike_1000x-1.jpg", 
     output_image_name = "res/bike_1000_res",
